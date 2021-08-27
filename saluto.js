a53230daabc4d4bf85f5ef1da3e76d75460c2cf8
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-08-26 20:19:17 -0700

    chore: added better documenting

--- saluto.js
+++ saluto.js
@@ -1,4 +1,7 @@
 export function saluto() {
+  // These are languages either I know,
+  // I want to learn, currently am learning,
+  // or used to be learning.
   const greetings = [
     "Hello", // English
     "Bonjour", // French
@@ -6,7 +9,10 @@ export function saluto() {
     "Hej", // Swedish
   ];
 
+  // Generate a random number using the length of the array
   let len = greetings.length;
   let i = Math.floor(Math.random() * len);
+  
+  // Return that greeting
   return greetings[i];
 }
