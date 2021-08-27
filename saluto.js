ac298554e30990449ad8fa96b8baf58f2648ea1e
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-08-26 20:19:34 -0700

    chore: add kr and cn; spacing changes

--- saluto.js
+++ saluto.js
@@ -3,8 +3,10 @@ export function saluto() {
   // I want to learn, currently am learning,
   // or used to be learning.
   const greetings = [
+    "你好", // Chinese (Simplifed/Mandarin)
     "Hello", // English
     "Bonjour", // French
+    "어이", // Korean
     "こんにちわ", // Japanese
     "Hej", // Swedish
   ];
@@ -12,7 +14,7 @@ export function saluto() {
   // Generate a random number using the length of the array
   let len = greetings.length;
   let i = Math.floor(Math.random() * len);
-  
+
   // Return that greeting
   return greetings[i];
 }
